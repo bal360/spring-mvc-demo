ccc3a7ced76b505d5a6445d7561ab8cd7c7a8e09
Blake A. Long <blake.a.long@gmail.com>
2020-09-25 21:30:30 -0600

    student form and controller @modelAttribute data binding

--- src/com/blakelong/springdemo/mvc/Student.java
+++ src/com/blakelong/springdemo/mvc/Student.java
@@ -5,9 +5,8 @@ public class Student {
 	private String firstName;
 	private String lastName;
 	
-	public Student(String firstName, String lastName) {
-		this.firstName = firstName;
-		this.lastName = lastName;
+	public Student() {
+		
 	}
 	
 	public String getFirstName() {
--- src/com/blakelong/springdemo/mvc/StudentController.java
+++ src/com/blakelong/springdemo/mvc/StudentController.java
@@ -2,18 +2,31 @@ package com.blakelong.springdemo.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/students")
+@RequestMapping("/student")
 public class StudentController {
 	
 	@RequestMapping("/showForm")
-	public String showForm(Model theModel) {
+	public String showForm(Model model) {
+		
 		// create new student object
+		Student student = new Student();
 		
 		// add student as model attribute
+		model.addAttribute("student", student);
 		
 		return "student-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm(@ModelAttribute("student") Student student) {
+		
+		// log the input data
+		System.out.println("First Name: " + student.getFirstName() + " " + student.getLastName());
+		
+		return "student-confirmation";
 	}
 }
